2ed5724ff20f0564c2f449f8d8663c4ebc5f0d65
Gearoid O'Treasaigh <gotreasa@users.noreply.github.com>
2023-11-28 13:55:09 +0000

    refactor: give squarring a number a clear name

--- src/ArraySquareRoot.ts
+++ src/ArraySquareRoot.ts
@@ -1,11 +1,15 @@
 const hasSquareRoot = (value: number): boolean =>
   Number.isInteger(Math.sqrt(value));
 
+const squareNumber = (value: number): number => {
+  return Math.pow(value, 2);
+};
+
 const toSquareOrRoot = (input: Array<number>): number[] => {
   return input.map((value) => {
     if (hasSquareRoot(value)) return Math.sqrt(value);
 
-    return Math.pow(value, 2);
+    return squareNumber(value);
   });
 };
 
