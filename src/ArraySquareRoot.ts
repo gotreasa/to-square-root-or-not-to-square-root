0ef99013f317e5772791e6a34a0c978d0195d727
Gearoid O'Treasaigh <gotreasa@users.noreply.github.com>
2023-11-28 13:51:52 +0000

    feat: should return [289, 1369, 169, 2, 3, 4] for the input [17, 37, 13, 4, 9, 16]

--- src/ArraySquareRoot.ts
+++ src/ArraySquareRoot.ts
@@ -1,11 +1,9 @@
 const toSquareOrRoot = (input: Array<number>): number[] => {
-  if (
-    JSON.stringify(input) === JSON.stringify([3, 2]) ||
-    JSON.stringify(input) === JSON.stringify([17, 37, 13])
-  )
-    return input.map((value) => Math.pow(value, 2));
+  return input.map((value) => {
+    if (Number.isInteger(Math.sqrt(value))) return Math.sqrt(value);
 
-  return input.map((value) => Math.sqrt(value));
+    return Math.pow(value, 2);
+  });
 };
 
 export default toSquareOrRoot;
