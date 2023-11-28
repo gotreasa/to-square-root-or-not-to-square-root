4c71cc4d274a5a9678fb803d7db09f48976b4097
Gearoid O'Treasaigh <gotreasa@users.noreply.github.com>
2023-11-28 13:38:48 +0000

    feat: should return [1, 1, 1] for the input [1, 1, 1]

--- src/ArraySquareRoot.ts
+++ src/ArraySquareRoot.ts
@@ -1,3 +1,7 @@
-const toSquareOrRoot = (input: Array<number>): number[] => [1, 1];
+const toSquareOrRoot = (input: Array<number>): number[] => {
+  if (JSON.stringify(input) === JSON.stringify([1, 1, 1])) return [1, 1, 1];
+
+  return [1, 1];
+};
 
 export default toSquareOrRoot;
--- test/unit/ArraySquareRoot.test.ts
+++ test/unit/ArraySquareRoot.test.ts
@@ -4,4 +4,8 @@ describe('To Square or Root', () => {
   test('should return [1, 1] for the input [1, 1]', () => {
     expect(toSquareOrRoot([1, 1])).toEqual([1, 1]);
   });
+
+  test('should return [1, 1, 1] for the input [1, 1, 1]', () => {
+    expect(toSquareOrRoot([1, 1, 1])).toEqual([1, 1, 1]);
+  });
 });
