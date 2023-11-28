044fd074fbb8d5d330d37bd053e9d608efe0512e
Gearoid O'Treasaigh <gotreasa@users.noreply.github.com>
2023-11-28 13:53:59 +0000

    refactor: extract out the calculation of square root

--- src/ArraySquareRoot.ts
+++ src/ArraySquareRoot.ts
@@ -1,6 +1,9 @@
+const hasSquareRoot = (value: number): boolean =>
+  Number.isInteger(Math.sqrt(value));
+
 const toSquareOrRoot = (input: Array<number>): number[] => {
   return input.map((value) => {
-    if (Number.isInteger(Math.sqrt(value))) return Math.sqrt(value);
+    if (hasSquareRoot(value)) return Math.sqrt(value);
 
     return Math.pow(value, 2);
   });
